930877a6db026d9641cf07931f714294fadbe73b
hectorgarciatw <dreamallica@gmail.com>
2024-07-04 13:54:59 -0300

    Modificando Display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,4 @@
-import data from '../data/codes.json';
+import data from "../data/codes.json";
 
 export default function Display({ keyPressed }) {
     // Filtrar el dato por keyPressed
@@ -9,14 +9,16 @@ export default function Display({ keyPressed }) {
             <div className="w-full gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
                 <div className="mx-auto rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
                     <div className="text-center">
-                        <h2 className="text-lg font-medium text-white">
-                            Information
-                            <span className="sr-only">Plan</span>
-                        </h2>
+                        <h2 className="text-lg font-medium text-white">keyCode</h2>
 
                         <p className="mt-2 sm:mt-4">
-                            <span className="text-3xl font-bold text-white sm:text-4xl">Key </span>
-                            <strong className="text-3xl font-bold text-white sm:text-4xl">{keyPressed}</strong>
+                            {filteredKey ? (
+                                <strong style={{ fontSize: "90px" }} className="font-bold text-white sm:text-4xl">
+                                    {filteredKey.keyCode}
+                                </strong>
+                            ) : (
+                                <strong>KeyCode not found</strong>
+                            )}
                         </p>
                     </div>
 
@@ -25,7 +27,15 @@ export default function Display({ keyPressed }) {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 text-indigo-700">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                             </svg>
-                            {filteredKey ? <span className="text-white">{`KeyCode: ${filteredKey.keyCode}`}</span> : <span className="text-white">KeyCode not found</span>}
+                            {filteredKey ? (
+                                <>
+                                    <span className="text-white block">{`Key pressed: ${filteredKey.key}`}</span>
+                                    <br />
+                                    <span className="text-white block">{`Code: ${filteredKey.code}`}</span>
+                                </>
+                            ) : (
+                                <span className="text-white">KeyCode not found</span>
+                            )}
                         </li>
                     </ul>
 
