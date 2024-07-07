dd0d143be9bcd290b6a646141dd324488803566a
hectorgarciatw <dreamallica@gmail.com>
2024-07-07 18:58:24 -0300

    Implementación de la versión mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,24 @@
-import Display from './Display';
+import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
+import Display from "./Display";
 
 export default function Content({ keyPressed }) {
+    const [key, setKey] = useState(null);
+
+    const handleKeyDown = (event) => {
+        setKey(event.key);
+    };
+
     return (
         <section className="bg-gray-900 text-white h-screen flex items-center">
-            <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center mt-[-250px]">
+            <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                 <div className="mx-auto max-w-3xl text-center">
-                    <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl">
+                    <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl">
                         getKeyCode
                         <span className="sm:block"> US standard 101 </span>
                     </h1>
-
-                    <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">Press the key you want to get the keycode for</p>
-
-                    <Display keyPressed={keyPressed} />
+                    {isMobile ? <input type="text" className="mt-6 border p-2 text-xl" onKeyDown={handleKeyDown} placeholder="Presiona una tecla" /> : <p className="mt-6 text-xl">Press the key you want to get the keycode for.</p>}
+                    <Display keyPressed={key || keyPressed} />
                 </div>
             </div>
         </section>
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,4 @@
-import data from '../data/codes.json';
+import data from "../data/codes.json";
 
 export default function Display({ keyPressed }) {
     // Filtrar el dato por keyPressed
@@ -9,11 +9,12 @@ export default function Display({ keyPressed }) {
             <div className="w-full gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
                 <div className="mx-auto rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
                     <div className="text-center">
+                        <div style={{ fontSize: "40px" }}>⌨️</div>
                         <h2 className="text-lg font-medium text-white">keyCode</h2>
 
                         <p className="mt-2 sm:mt-4">
                             {filteredKey ? (
-                                <strong style={{ fontSize: '90px' }} className="font-bold text-white sm:text-4xl">
+                                <strong style={{ fontSize: "90px" }} className="font-bold text-white sm:text-4xl">
                                     {filteredKey.keyCode}
                                 </strong>
                             ) : (
@@ -28,10 +29,6 @@ export default function Display({ keyPressed }) {
                             <h2 className="text-lg font-medium text-white">{`Code: ${filteredKey.code}`}</h2>
                         </>
                     ) : null}
-
-                    <a href="#" className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
-                        More info
-                    </a>
                 </div>
             </div>
         </div>
