bfbdac5fd1862d0133777ae8dd4144b961a670c0
hectorgarciatw <dreamallica@gmail.com>
2024-10-07 09:04:53 -0300

    Agregado del input en la versión mobile para la presión de una tecla

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,8 +7,8 @@ export default function Content({ keyPressed }) {
     const inputRef = useRef(null);
 
     useEffect(() => {
-        if (isMobile) {
-            // Enfocar el input automáticamente en móviles al cargar la página
+        if (isMobile && inputRef.current) {
+            // Enfocar el input automáticamente en móviles
             inputRef.current.focus();
         }
     }, []);
@@ -16,13 +16,19 @@ export default function Content({ keyPressed }) {
     const handleKeyDown = (event) => {
         if (isMobile) {
             setKey(event.key);
-            // Limpia el input tras presionar una tecla
             event.target.value = "";
         }
     };
 
+    const handleTouchStart = () => {
+        if (isMobile && inputRef.current) {
+            // Forzar enfoque en el input cuando se toca la pantalla
+            inputRef.current.focus();
+        }
+    };
+
     return (
-        <section className="bg-gray-900 text-white h-screen flex items-center">
+        <section className="bg-gray-900 text-white h-screen flex items-center" onTouchStart={handleTouchStart}>
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                 <div className="mx-auto max-w-3xl text-center">
                     <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl">
@@ -32,13 +38,7 @@ export default function Content({ keyPressed }) {
                     {isMobile ? (
                         <div className="mt-6 text-xl">
                             <p>Toca la pantalla y presiona una tecla</p>
-                            <input ref={inputRef} type="text" className="hidden" onKeyDown={handleKeyDown} aria-label="Tecla presionada" />
-                            <div
-                                className="editable-area"
-                                // Forzar enfoque en móviles
-                                onTouchStart={() => inputRef.current.focus()}
-                                style={{ height: "1px", width: "1px", opacity: 0 }}
-                            ></div>
+                            <input ref={inputRef} type="text" className="text-center mt-4 border p-2 text-xl opacity-50" onKeyDown={handleKeyDown} placeholder="Presiona una tecla" aria-label="Tecla presionada" />
                         </div>
                     ) : (
                         <p className="mt-6 text-xl">Press the key you want to get the keycode for.</p>
