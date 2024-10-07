a7a82adf2bd2e2547f69e4cdadc99de0ca6b9a89
Héctor <dreamallica@gmail.com>
2024-10-07 12:12:08 -0300

    Alterando estilo de la versión mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { isMobile } from "react-device-detect";
-import Display from "./Display";
+import React, { useState, useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
+import Display from './Display';
 
 export default function Content({ keyPressed }) {
     const [key, setKey] = useState(null);
+    // Para manejar la visibilidad del teclado
+    const [keyboardActive, setKeyboardActive] = useState(false);
 
     const handleInput = (event) => {
         const input = event.target.value;
@@ -15,31 +17,35 @@ export default function Content({ keyPressed }) {
     const handleKeyDown = (event) => {
         if (isMobile) {
             setKey(event.key);
-            // Limpiar el campo de entrada después de presionar la tecla
-            event.target.value = "";
+            event.target.value = '';
         }
     };
 
-    // Efecto para ajustar el scroll cuando el teclado virtual aparece
     useEffect(() => {
         const adjustForKeyboard = () => {
-            window.scrollTo(0, document.body.scrollHeight);
+            // Detecta que el teclado está activo
+            setKeyboardActive(true);
+        };
+
+        const resetForKeyboard = () => {
+            // Detecta que el teclado está inactivo
+            setKeyboardActive(false);
         };
         // Cuando aparece el teclado
-        window.addEventListener("focusin", adjustForKeyboard);
+        window.addEventListener('focusin', adjustForKeyboard);
         // Cuando desaparece
-        window.addEventListener("focusout", () => window.scrollTo(0, 0));
+        window.addEventListener('focusout', resetForKeyboard);
 
         return () => {
-            window.removeEventListener("focusin", adjustForKeyboard);
-            window.removeEventListener("focusout", () => window.scrollTo(0, 0));
+            window.removeEventListener('focusin', adjustForKeyboard);
+            window.removeEventListener('focusout', resetForKeyboard);
         };
     }, []);
 
     return (
-        <section className="bg-gray-900 text-white h-screen flex items-center">
+        <section className={`bg-gray-900 text-white h-screen flex items-center ${keyboardActive ? 'keyboard-active' : ''}`}>
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-                <div className="mx-auto max-w-3xl text-center">
+                <div className={`mx-auto max-w-3xl text-center ${keyboardActive ? 'keyboard-adjust' : ''}`}>
                     <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl">
                         getKeyCode
                         <span className="sm:block"> US standard 101 </span>
